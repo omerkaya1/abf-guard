661c60800b4ebbb96728db665f11e74805118dcc
Omer Kaya <omerkaya513@gmail.com>
2019-11-27 13:18:13 +0300

    [Add] Finished ITs:
    1) Added ITs functions;
    2) Reworked the feature file;
    3) Corrected IT's docker-compose.test file.

--- test/integration-test/features/api.feature
+++ test/integration-test/features/api.feature
@@ -3,33 +3,31 @@
 ########################################
 #  1) Add an IP to the whitelist;
 #  2) Add an IP to the blacklist;
-#  3) Delete an IP from the whitelist
-#  4) Delete an IP from the blacklist
-#  5) Get the list of IPs from the whitelist
-#  6) Get the list of IPs from the whitelist
-#  7) Send an authorise request
-#  8) Send a flush buckets request
-#  9) Send a purge bucket request
-#  10) Error handling
+#  3) Check happy path for a subnet;
+#  4) Check blocking for a subnet;
+#  5) Normal authorisation;
+#  6) Send a flush buckets request;
+#  7) Send a purge buckets request;
+#  8) Get the list of IPs from the whitelist;
+#  9) Get the list of IPs from the blacklist;
+#  10) Delete an IP from the whitelist
+#  11) Delete an IP from the blacklist
 ########################################
 Feature: Functional ABFG API
 	In order to test the abf-guard application
 	As a user that operates with the service through API
 	The service should allow the following
 
-	# NOTE: Checked. Ready to be used.
 	Scenario: Add an IP to the whitelist
 		When we send a request to add "10.0.0.1" subnet to the "white" list
 		Then the request is completed without errors
 		And the "10.0.0.1" ip is "" in the "white" list
 
-	# NOTE: Checked. Ready to be used.
 	Scenario: Add an IP to the blacklist
 		When we send a request to add "10.0.0.2" subnet to the "black" list
 		Then the request is completed without errors
 		And the "10.0.0.2" ip is "" in the "black" list
 
-    # NOTE: Checked. Ready to be used.
 	Scenario: Check happy path for a subnet
 		When we send "white" authorisation requests for "2" times of the allowed limits with parameters:
 			"""
@@ -37,7 +35,6 @@ Feature: Functional ABFG API
 			"""
 		Then they all succeed
 
-	# NOTE: Checked. Ready to be used.
 	Scenario: Check blocking for a subnet
 		When we send "black" authorisation requests for "2" times of the allowed limits with parameters:
 			"""
@@ -45,15 +42,13 @@ Feature: Functional ABFG API
 			"""
 		Then they all fail
 
-	# NOTE: Checked. Ready to be used.
 	Scenario: Normal authorisation
 		When we send "normal" authorisation requests for "2" times of the allowed limits with parameters:
 			"""
 			{ "login": "Morty", "password": "1234", "ip": "10.0.0.3" }
 			"""
-		Then half of the requests pass and half do not
+		Then precisely "5" of the requests should have passed and "5" should not have passed
 
-	# NOTE: Checked. Ready to be used.
 	Scenario: Send a flush buckets request
 		Given we send an authorisation request with parameters:
 			"""
@@ -63,9 +58,8 @@ Feature: Functional ABFG API
 			"""
 			{ "login": "Morty", "password": "", "ip": "10.0.0.3" }
 			"""
-		Then half of the requests pass and half do not
+		Then precisely "1" of the requests should have passed and "1" should not have passed
 
-	# NOTE: Checked. Ready to be used.
 	Scenario: Send a purge bucket request
 		Given we send an authorisation request with parameters:
 			"""
@@ -75,24 +69,20 @@ Feature: Functional ABFG API
 		"""
 			{ "login": "Morty", "password": "1234", "ip": "10.0.0.3" }
 			"""
-		Then half of the requests pass and half do not
+		Then precisely "3" of the requests should have passed and "3" should not have passed
 
-	# NOTE: Checked. Ready to be used.
 	Scenario: Get a list of IPs that belong to the whitelist
 		When we send a request to get a list of ips from "white" list
 		Then the "10.0.0.1" ip is "" in the "white" list
 
-	# NOTE: Checked. Ready to be used.
 	Scenario: Get a list of IPs that belong to the blacklist
 		When we send a request to get a list of ips from "black" list
 		Then the "10.0.0.2" ip is "" in the "black" list
 
-	# NOTE: Checked. Ready to be used.
 	Scenario: Delete an IP from the whitelist
 		When we send a request to delete "10.0.0.1" ip from "white" list
 		Then the request is completed without errors
 
-	# NOTE: Checked. Ready to be used.
 	Scenario: Delete an IP from the blacklist
 		When we send a request to delete "10.0.0.2" ip from "black" list
 		Then the request is completed without errors
--- test/integration-test/main_test.go
+++ test/integration-test/main_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"github.com/DATA-DOG/godog"
 	"github.com/DATA-DOG/godog/gherkin"
@@ -20,9 +19,9 @@ import (
 )
 
 type authorise struct {
-	Login string `json:"login:omitempty"`
-	Pwd   string `json:"pwd:omitempty"`
-	IP    string `json:"ip:omitempty"`
+	Login string `json:"login,omitempty"`
+	Pwd   string `json:"password,omitempty"`
+	IP    string `json:"ip,omitempty"`
 }
 
 type testABFG struct {
@@ -79,8 +78,7 @@ func getNameByIndex(index int, auth authorise) string {
 }
 
 func newTestABFG() *testABFG {
-	//conn, err := grpc.Dial(os.Getenv("TEST_HOST") + ":" + os.Getenv("TEST_PORT"), grpc.WithInsecure())
-	conn, err := grpc.Dial("localhost:6666", grpc.WithInsecure())
+	conn, err := grpc.Dial(os.Getenv("TEST_HOST")+":"+os.Getenv("TEST_PORT"), grpc.WithInsecure())
 	fastFail(err)
 	client := api.NewABFGuardClient(conn)
 	loginLimit, err := strconv.Atoi(os.Getenv("LOGIN_LIMIT"))
@@ -107,17 +105,16 @@ func (abfg *testABFG) weSendARequestToAddSubnetToTheList(arg1, arg2 string) erro
 
 	resp, err := abfg.c.AddIPToWhitelist(ctx, &api.SubnetRequest{Ip: arg1, List: blacklistCheck(arg2)})
 	if err != nil {
-		log.Println(err)
 		return err
 	}
-	abfg.latestOk = resp.GetOk()
-	abfg.latestErr = errors.New(resp.GetError())
+	if resp.GetError() != "" && resp.GetOk() {
+		abfg.latestErr = fmt.Errorf("last returned error: %s and %v", resp.GetError(), resp.GetOk())
+	}
 	return nil
 }
 
 func (abfg *testABFG) theRequestIsCompletedWithoutErrors() error {
 	if abfg.latestErr != nil {
-		log.Println(abfg.latestErr)
 		return abfg.latestErr
 	}
 	return nil
@@ -127,7 +124,6 @@ func (abfg *testABFG) theIpIsInTheList(arg1, arg2, arg3 string) error {
 	ctx, _ := context.WithTimeout(context.Background(), time.Second*5)
 	resp, err := abfg.c.GetIPList(ctx, &api.ListRequest{ListType: blacklistCheck(arg3)})
 	if err != nil {
-		log.Println(err)
 		return err
 	}
 	if resp.GetError() != "" {
@@ -139,15 +135,15 @@ func (abfg *testABFG) theIpIsInTheList(arg1, arg2, arg3 string) error {
 	if arg2 != "" {
 		for _, v := range resp.GetIps().GetList() {
 			if v == arg1 {
-				return fmt.Errorf("the %s address is STILL in the %s list", arg1, arg3)
+				return fmt.Errorf("\033[34mthe %s address is in the %s list\033[0m\n", arg1, arg3)
 			}
 		}
-		log.Printf("the %s address is %s in the %s list", arg1, arg2, arg3)
+		log.Printf("the %s address is %s in the %s list\n", arg1, arg2, arg3)
 		return nil
 	} else {
 		for _, v := range resp.GetIps().GetList() {
 			if v == arg1 {
-				fmt.Printf("the %s address is in the %s list", arg1, arg3)
+				fmt.Printf("\033[34mthe %s address is in the %s list\033[0m\n", arg1, arg3)
 				return nil
 			}
 		}
@@ -216,6 +212,7 @@ func (abfg *testABFG) theyAllSucceed() error {
 	for i := range abfg.respOkMap {
 		delete(abfg.respOkMap, i)
 	}
+	fmt.Printf("\033[34m successfuly cleaned the respOkMap: %v\033[0m\n", abfg.respOkMap)
 	return nil
 }
 
@@ -228,25 +225,34 @@ func (abfg *testABFG) theyAllFail() error {
 	for i := range abfg.respOkMap {
 		delete(abfg.respOkMap, i)
 	}
+	fmt.Printf("\033[34m successfuly cleaned the respOkMap: %v\033[0m\n", abfg.respOkMap)
 	return nil
 }
 
-func (abfg *testABFG) halfOfTheRequestsPassAndHalfDoNot() error {
-	half := len(abfg.respOkMap)
-	if half != abfg.loginLimit/2 {
-		return fmt.Errorf("the values do not match: half = %d and actual half = %d", half, abfg.loginLimit/2)
+func (abfg *testABFG) preciselyOfTheRequestsShouldHavePassedAndShouldNotHavePassed(arg1, arg2 string) error {
+	passed, err := strconv.Atoi(arg1)
+	if err != nil {
+		return err
 	}
-	for i, v := range abfg.respOkMap {
-		switch {
-		case i <= half:
-			if !v {
-				return fmt.Errorf("%d request failed", i)
-			}
-		default:
-			if v {
-				return fmt.Errorf("%d request DID NOT fail", i)
-			}
+	failed, err := strconv.Atoi(arg2)
+	if err != nil {
+		return err
+	}
+	actualPassed, actualFailed := 0, 0
+	for _, v := range abfg.respOkMap {
+		if v {
+			actualPassed++
+		} else {
+			actualFailed++
 		}
+	}
+	if actualPassed != passed {
+		return fmt.Errorf("values of actualPassed and expected to pass requests do not match: %d and %d",
+			actualPassed, passed)
+	}
+	if actualFailed != failed {
+		return fmt.Errorf("values of actualFailed and expected to fail requests do not match: %d and %d",
+			actualFailed, failed)
 	}
 	for i := range abfg.respOkMap {
 		delete(abfg.respOkMap, i)
@@ -284,7 +290,7 @@ func (abfg *testABFG) sendingRequestTimesForTheFollowingBuckets(arg1, arg2 strin
 	if err := json.Unmarshal([]byte(cleanJson), a); err != nil {
 		return err
 	}
-	ctx, _ := context.WithTimeout(context.Background(), time.Second*5)
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*10)
 
 	for i := 0; i < times; i++ {
 		switch arg1 {
@@ -373,7 +379,8 @@ func FeatureContext(s *godog.Suite) {
 		test.weSendAuthorisationRequestsForTimesOfTheAllowedLimitsWithParameters)
 	s.Step(`^they all succeed$`, test.theyAllSucceed)
 	s.Step(`^they all fail$`, test.theyAllFail)
-	s.Step(`^half of the requests pass and half do not$`, test.halfOfTheRequestsPassAndHalfDoNot)
+	s.Step(`^precisely "([^"]*)" of the requests should have passed and "([^"]*)" should not have passed$`,
+		test.preciselyOfTheRequestsShouldHavePassedAndShouldNotHavePassed)
 	s.Step(`^we send an authorisation request with parameters:$`, test.weSendAnAuthorisationRequestWithParameters)
 	s.Step(`^sending "([^"]*)" request "([^"]*)" times for the following buckets:$`,
 		test.sendingRequestTimesForTheFollowingBuckets)
@@ -385,7 +392,7 @@ func FeatureContext(s *godog.Suite) {
 
 func TestMain(m *testing.M) {
 	fmt.Println("Waiting for all services to become available...")
-	time.Sleep(time.Second * 15)
+	time.Sleep(time.Second * 10)
 
 	status := godog.RunWithOptions("API", func(s *godog.Suite) {
 		FeatureContext(s)
